d27e93f2045b4699f1fafbdf581cf2ebd6dee49e
CIoramo <ciara@Latitude3500>
2026-03-05 13:30:47 +1300

    commit to github repository

--- BIT504_A2_Task9Project/src/Main.java
+++ BIT504_A2_Task9Project/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Task 9 - Initial version again");
+		System.out.println("Task 9 - Initial version take two");
 
 	}
 
